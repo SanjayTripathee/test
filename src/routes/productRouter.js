f47cfa5d82cd39bb64775bba9c05c981118ca5c6
sanjay <st1706851@gmail.com>
2025-03-16 14:18:25 +0545

    ok

--- src/routes/productRouter.js
+++ src/routes/productRouter.js
@@ -1,46 +1,20 @@
-// import { Router } from "express";
-// import {
-//   crateProductController,
-//   deleteProductController,
-//   readAllProductController,
-//   readSpecificProductController,
-//   updateProductController,
-// } from "../controller/productController.js";
-
-// let productRouter = Router();
-// productRouter
-//   .route("/")
-//   .post(crateProductController)
-//   .get(readAllProductController);
-// productRouter
-//   .route("/:id")
-//   .get(readSpecificProductController)
-//   .patch(updateProductController)
-//   .delete(deleteProductController);
-// export default productRouter;
-
-
-
 import { Router } from "express";
 import {
-  createProductController,
+  crateProductController,
   deleteProductController,
   readAllProductController,
   readSpecificProductController,
   updateProductController,
 } from "../controller/productController.js";
 
-const productRouter = Router();
-
-// Routes for all products
-productRouter.route("/")
-  .post(createProductController)
+let productRouter = Router();
+productRouter
+  .route("/")
+  .post(crateProductController)
   .get(readAllProductController);
-
-// Routes for a specific product by ID
-productRouter.route("/:id")
+productRouter
+  .route("/:id")
   .get(readSpecificProductController)
   .patch(updateProductController)
   .delete(deleteProductController);
-
 export default productRouter;
