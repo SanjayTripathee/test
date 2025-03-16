4fc4b258a1f9d21bb2105f10500c737f6b8bb2c2
sanjay <st1706851@gmail.com>
2025-03-16 14:30:58 +0545

    changessss

--- src/connectDB/connectToMongooDb.js
+++ src/connectDB/connectToMongooDb.js
@@ -1,11 +1,32 @@
+// import mongoose from "mongoose";
+
+// const connectToMongoDb = () => {
+//   mongoose.connect(
+//     "mongodb+srv://sanjay:sanjay@codersanjay.jknb2.mongodb.net/dw18project"
+//   );
+//   // mongoose.connect("mongodb://0.0.0.0:27017/self");
+//   console.log("application is connected to MongoDB successfully");
+// };
+
+// export default connectToMongoDb;
+
+
+
 import mongoose from "mongoose";
 
-const connectToMongoDb = () => {
-  mongoose.connect(
-    "mongodb+srv://sanjay:sanjay@codersanjay.jknb2.mongodb.net/dw18project"
-  );
-  // mongoose.connect("mongodb://0.0.0.0:27017/self");
-  console.log("application is connected to MongoDB successfully");
+let isConnected = false;
+
+const connectToMongoDb = async () => {
+  if (isConnected) return;
+
+  try {
+    const conn = await mongoose.connect("mongodb+srv://sanjay:sanjay@codersanjay.jknb2.mongodb.net/dw18project");
+
+    isConnected = true;
+    console.log("MongoDB Connected");
+  } catch (error) {
+    console.error("MongoDB connection failed", error);
+  }
 };
 
 export default connectToMongoDb;
